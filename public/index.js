75f66ae7fbc947ecf8a2da89bebd66836e5c9832
Joey Lim <jyorin0206@gmail.com>
2021-11-18 17:04:15 +0800

    changed port

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const tempSpan = document.getElementById("temp")
 const tempWarnSpan = document.getElementById("temp_warn")
 const successStatusSpan = document.getElementById("success_status")
 const boxDiv = document.getElementById("box")
-const socket = new WebSocket('ws://0.0.0.0:8088')
+const socket = new WebSocket('ws://0.0.0.0:80')
 socket.addEventListener('message', function(event) {
     const reader = new FileReader()
     reader.addEventListener('load',(event2) => {
